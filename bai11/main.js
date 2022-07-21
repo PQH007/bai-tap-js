2f0cb12026590d63b8a040ff348a6392ec0d4e9b
PQHtobi <phanquochung2511@gmail.com>
2022-07-22 01:17:16 +0700

    bai11

--- bai11/main.js
+++ bai11/main.js
@@ -2,3 +2,38 @@ $('#my-tabs').tabs({
     collapsible:true,
     // event:'mouseover',
 });
+$('#my-spinner').spinner({
+    min: 0,
+    max: 100,
+    step:5,
+    // stop: function (){
+    //     var vl = $(this).spinner('value');
+    //     $('#spinner_value').html(vl);
+    // },
+    change: function (){
+        var vl = $(this).spinner('value');
+        $('#spinner_value').html(vl);
+    }
+});
+
+// slider
+$('#mySlider').slider({
+    min: 0,
+    max: 100,
+    step: 1,
+    stop: function (){
+        var vl = $(this).slider('value');
+        $('#sp_value').html(vl);
+    }
+
+});
+$('#mySlider2').slider({
+    orientation: 'vertical',
+    min: 0,
+    max: 100,
+    step: 1,
+    stop: function (){
+        var vl = $(this).slider('value');
+        $('#sp_value2').html(vl);
+    }
+});
